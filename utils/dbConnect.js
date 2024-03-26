41a6e9c076ee5ceb469c827a7f1002b5dd6b7f00
ritesh-blog27 <riteshminecraft27@gmail.com>
2024-03-26 17:29:25 +0530

    little testing work

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -36,7 +36,7 @@ const query = (text, params) => {
 			database: process.env.PGDATABASE,
 			password: process.env.PGPASSWORD,
 			port: process.env.PGPORT,
-			max: 10
+			max: 30
 		});
 	
 		global.db.pool.on('error', (err) => {
