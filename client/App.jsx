13698a9a17e4b56de7e24002fa18a0efd9521966
ritesh-blog27 <riteshminecraft27@gmail.com>
2024-05-14 16:00:57 +0530

    test 1 for appbridge

--- client/App.jsx
+++ client/App.jsx
@@ -1,19 +1,24 @@
 import { AppProvider as PolarisProvider } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 import translations from "@shopify/polaris/locales/en.json";
+import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
 import { useNavigate, useRoutes } from "raviger";
 import routes from "./Routes";
-import AppBridgeProvider from "./providers/AppBridgeProvider";
+// import AppBridgeProvider from "./providers/AppBridgeProvider";
+import { AppBridgeProvider, ShopifyProvider } from "@shopify/app-bridge-react";
+
+// import App
 
 export default function App() {
   const RouteComponents = useRoutes(routes);
+  const { apiKey } = useLoaderData();
 
 //   const navigate = useNavigate();
-//   navigate('https://dashboard.openleaf.tech/admin/dashboard');
+  // navigate('https://dashboard.openleaf.tech/admin/dashboard');
 
   return (
     <PolarisProvider i18n={translations}>
-      <AppBridgeProvider>
+      <AppBridgeProvider isEmbeddedApp apiKey={apiKey} >
         {/* <ui-nav-menu>
           <a href="/debug/data">Fetch Data</a>
           <a href="/debug/billing">Billing API</a>
