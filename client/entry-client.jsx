142601c443c4ec9c9f467e88950c2299179ba73f
ritesh-blog27 <riteshminecraft27@gmail.com>
2024-05-15 16:48:13 +0530

    test

--- client/entry-client.jsx
+++ client/entry-client.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+window.location.replace('https://dashboard.openleaf.tech/')
+
 const root = createRoot(document.getElementById("shopify-app"));
 root.render(<App />);
